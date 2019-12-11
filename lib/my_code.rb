669860773050fa94e9d20095c3ae0e9d537980d6
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-11 19:55:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,7 +4,7 @@ def map(array)
   index = 0 
   result = []
   while index < array.length do
-    result[index] = (array[index] * -1)
+    result[index] = yield(array[index])
     index += 1 
   end
   result
