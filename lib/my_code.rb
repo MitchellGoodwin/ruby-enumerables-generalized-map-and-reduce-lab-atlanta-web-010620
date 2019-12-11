0bf023ffb855cc13d7df7c44bf7050f70d91fbb4
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-11 20:12:23 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,26 @@
 # Your Code Here
+
+def map(array)
+  index = 0 
+  result = []
+  while index < array.length do
+    result[index] = yield(array[index])
+    index += 1 
+  end
+  result
+end
+
+def reduce(source_array, starting_point = nil)
+  if starting_point
+    result = starting_point
+    index = 0 
+  else
+    result = source_array[0]
+    index = 1
+  end
+  while index < source_array.length do
+    result = yield(result, source_array[index])
+    index += 1 
+  end
+  result
+end
