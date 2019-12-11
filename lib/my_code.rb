c2b18b958431488636291ed38f1dbbe1503db481
Mitchell Goodwin <mgoodwin777@gmail.com>
2019-12-11 19:56:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,3 +10,12 @@ def map(array)
   result
 end
 
+def reduce(source_array, starting_point = 0)
+  index = 0 
+  result = starting_point
+  while index < source_array.length do
+    result += source_array[index]
+    index += 1 
+  end
+  result
+end
